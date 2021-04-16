bcf5c7a2874ebae412e4db38eb75b00f2eb43afa
imvickykumar999 <imvickykumar999@gmail.com>
2021-04-17 01:11:08 +0530

    uploaded using vixuploader

---
+++ vixuploader/instaphoto.py
@@ -0,0 +1,14 @@
+from instabot import Bot
+bot = Bot()
+
+def upload():
+    user = input('Enter Username : ')
+    passwd = input('Enter Password : ')
+
+    bot.login(username = user, password = passwd)
+
+    path = input('Photo name.jpg : ')
+    cap = input('Enter Caption : ')
+
+    bot.upload_photo(path, caption = cap)
+    print("\n Congratulations, Photo is Uploaded... ") # photo.png.REMOVE_ME
